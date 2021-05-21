adebf70a13e866f71e84a2cca12060f38ab201b0
SETING2021 <sechavarri1983@gmail.com>
2021-05-21 14:22:08 -0500

    :construction: Modificamos la version de openjdk para que jale en Openshift

--- src/main/java/com/example/seting/demo/api/PersonController.java
+++ src/main/java/com/example/seting/demo/api/PersonController.java
@@ -4,7 +4,6 @@ import com.example.seting.demo.model.Person;
 import com.example.seting.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.theme.CookieThemeResolver;
 
 import java.util.List;
 import java.util.UUID;
