41f9c5fed1f011c5b457f38699a546328376c18a
SETING2021 <sechavarri1983@gmail.com>
2021-05-21 14:24:40 -0500

    :construction: Modificamos la version de openjdk para que jale en Openshift, agregamos Dockerfile y log4j2

--- src/main/java/com/example/seting/demo/api/PersonController.java
+++ src/main/java/com/example/seting/demo/api/PersonController.java
@@ -4,15 +4,21 @@ import com.example.seting.demo.model.Person;
 import com.example.seting.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.theme.CookieThemeResolver;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // el nombre del servicio que se va a llamar con JSON
 @RequestMapping ("api/v1/person")
 @RestController
 public class PersonController {
+	
+	/**
+	 * Manejo de logs.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(PersonController.class.getName());
 
     private final PersonService personService;
 
@@ -23,7 +29,7 @@ public class PersonController {
 
     @PostMapping
     public void addPerson(@RequestBody Person person){
-
+    	LOGGER.log(Level.INFO, "Add person with name : " + person.getName());
         personService.addPerson(person);
     }
 
